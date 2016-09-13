503e425b3249ecc50d63e577e0d84ededa2a538e
xuxingzhuang <hzxuxingzhuang@163.com>
2016-09-13 16:06:20 +0800

    Egit input.sh file script

--- input.sh
+++ input.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+while :
+do
 read -p "Please input your name:" Name
-echo $Name
+if [ "$Name" = "quit" ]; then
+    exit 4
+elif [ "$Name" = "exit" ]; then
+    exit 4
+else
+   echo  "You input ${#Name} number...."
+fi
+done
+
